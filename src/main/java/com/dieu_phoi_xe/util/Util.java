aa1306d8903eeacef5bc809541a8841fa313cbd2
PG6402 <tranphat701@gmail.com>
2024-04-03 16:06:34 +0700

    update

--- src/main/java/com/dieu_phoi_xe/util/Util.java
+++ src/main/java/com/dieu_phoi_xe/util/Util.java
@@ -64,7 +64,7 @@ public class Util {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).setHost("demo-dpx.onrender.com").build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
